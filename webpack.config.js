9df7157fe04dc7cd1161d386a79d6fe3b00ea35f
Marius <mariusvilholm08@gmail.com>
2019-03-28 11:05:43 +0100

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, '/src/app/index.js'), // webpack entry point. Module to start building dependency graph
+
+    output: {
+        path: path.resolve(__dirname, 'dist'), // folder to store bundle
+        filename: '[name].bundle.js', // name of bundle
+        publicPath: '/' // public URL of the output directory when referenced in a browser
+    },
+
+    module: { // where we defined file patterns and their loaders
+        rules: []
+    },
+
+    plugins: [ // array of plugins to apply to build chunk
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '/public/index.html'),
+            inject: 'body'
+        }),
+
+        // clean dist folder
+        new CleanWebpackPlugin(['dist'], {
+            'verbose': true // write logs to console
+        })
+    ],
+
+    devServer: {
+        contentBase: './dist', // source of static assets
+        port: 8080
+    }
+};
